f6d0e5c18ee6e226fb545b551bd1a02d2033ee5f
itzamacko <anthony.marchante@me.com>
2012-07-16 15:55:15 -0400

    updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '8f8a302b63f97ec9c8d7483fe4edaa0629011736185f8ba5bd639e2a7640ca93a276f0b01f954d4951937e891e34354fae19e5e4d2a32c811b9db1e15b8f1316'
+Metamuna::Application.config.secret_token = '8f8a302b63f97ec9c8d7483fe4edaa0629011736185f8ba5bd639e2a7640ca93a276f0b01f954d4951937e891e34354fae19e5e4d2a32c811b9db1e15b8f1316'
