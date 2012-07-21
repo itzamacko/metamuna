d0cd718c4956b076c5ffc4f3671ee71f90b5f456
Itzamacko <anthony.marchante@me.com>
2012-07-21 13:13:58 -0400

    Finish layout and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Metamuna::Application.routes.draw do
   
-  root to: 'static_pages#home'
+  get "users/new"
 
+  root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
 
